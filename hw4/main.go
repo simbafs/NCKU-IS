96b5d3af70b82875967db55d9ed28b5e0e1d9d1e
simba-fs <simba.fs@gmail.com>
2022-10-04 21:48:20 +0800

    try to use goroutine to speed up, but failed

--- hw4/main.go
+++ hw4/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"sync"
+	"sync/atomic"
 
 	"github.com/schollz/progressbar/v3"
 )
@@ -21,8 +23,7 @@ func genKey(a, b, c, d rune) []rune {
 	return []rune(fmt.Sprintf("Hj%cN)%ctgZ%c9wrc%cm", a, b, c, d))
 }
 
-func resolve(from, to rune, add func(int) error, ptf, keyf *os.File) {
-	index := 0
+func resolve(wg *sync.WaitGroup, from, to rune, add func(int) error, index *int64, ptf, keyf *os.File) {
 	for a := from; a < to; a++ {
 		for b := rune(0); b < max; b++ {
 			for c := rune(0); c < max; c++ {
@@ -32,13 +33,15 @@ func resolve(from, to rune, add func(int) error, ptf, keyf *os.File) {
 					// fmt.Println([]byte(string(key)))
 					pt := DecryptAES([]byte(string(key)), ct)
 					if pt[:2] == prefix {
-						fmt.Fprintln(ptf, index, Unpadding(pt))
-						fmt.Fprintln(keyf, index, key)
+						fmt.Fprintln(ptf, *index, Unpadding(pt))
+						fmt.Fprintln(keyf, *index, key)
+						atomic.AddInt64(index, 1)
 					}
 				}
 			}
 		}
 	}
+	wg.Done()
 }
 
 func main() {
@@ -48,5 +51,22 @@ func main() {
 	keyf, err := os.OpenFile("key", fo, 0644)
 	CheckError(err)
 
-	resolve(rune(0), rune(max), bar.Add, ptf, keyf)
+	index := int64(0)
+
+	resolve(nil, rune(0), rune(max), bar.Add, &index, ptf, keyf)
+	// wg := &sync.WaitGroup{}
+	// n := 1
+	// d := max / n
+	// wg.Add(n)
+	// for i := 0; i < n; i++ {
+	//     if i == n-1 {
+	//         // fmt.Println(d*i, max)
+	//         go resolve(nil, rune(d*i), rune(max), bar.Add, &index, ptf, keyf)
+	//     } else {
+	//         // fmt.Println(d*i, d*i+d)
+	//         go resolve(nil, rune(d*i), rune(d*i+d), bar.Add, &index, ptf, keyf)
+	//     }
+	// }
+	// fmt.Println(N)
+	// wg.Wait()
 }
